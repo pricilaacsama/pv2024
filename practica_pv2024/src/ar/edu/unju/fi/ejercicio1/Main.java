d14082c147745c68ebffbe3708bdb79a59299804
prici <prici@prici>
2024-04-05 20:53:54 -0300

    subo ejercicio1 del tp 1

---
+++ practica_pv2024/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int Num1 = 8;
+		int Num2 = 5;
+		int Num3 = 1;
+		int Num4 = 20;
+		int Num5 = 9;
+		
+		double promedio = (Num1 + Num2 + Num3 + Num4 + Num5)/5.0;
+		
+		System.out.println("el promedio es: " + promedio);
+		
+	}
+
+}
